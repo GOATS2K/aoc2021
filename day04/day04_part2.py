bb2b8f2e9cf2a7f146d9f6e1d357834948870e0f
GOATS2K <jp@glattetre.com>
2021-12-10 19:56:59 +0000

    feat(day04, part 2): incomplete solution that passes test but not actual problem :C

---
+++ day04/day04_part2.py
@@ -0,0 +1,119 @@
+class Bingo:
+    def __init__(self) -> None:
+        self.boards = self.construct_boards()
+        self.input = self.get_puzzle_input()
+
+    def get_puzzle_input(self) -> list[str]:
+        with open("day04/test_input.txt", "r") as infile:
+            lines = []
+            for line in infile.readlines():
+                new_line = line.rstrip("\n")
+                lines += [int(line) for line in new_line.split(",")]
+        return lines
+
+    def construct_boards(self) -> dict[int, list[list[str]]]:
+        with open("day04/test_boards.txt", "r") as infile:
+            board_data = [line.rstrip("\n") for line in infile.readlines()]
+
+        boards = {}
+
+        blank_line_counter = 1
+        for line in board_data:
+            if line:
+                line = line.rstrip()
+                if not boards.get(blank_line_counter):
+                    boards[blank_line_counter] = []
+                tiles = line.split(",")
+                if len(tiles) != 5:
+                    print(f"Board {blank_line_counter} is malformed.")
+                boards[blank_line_counter].append([int(tile) for tile in tiles])
+            else:
+                blank_line_counter += 1
+        return boards
+
+    def find_number_on_board(self, number: int) -> dict[int, list[int]]:
+        boards_found = {}
+        for board_number, board in self.boards.items():
+            for i in range(5):
+                for j in range(5):
+                    if board[i][j] == number:
+                        if not boards_found.get(board_number):
+                            boards_found[board_number] = []
+                        boards_found[board_number].append({"row": i, "column": j})
+
+        return boards_found
+
+    def scan_column(self, board: list[list[int]], board_number):
+        last_column = 0
+        column = []
+        for i in range(5):
+            for j in range(5):
+                if board[j][i] == -1:
+                    if last_column > j:
+                        column = []
+                    column.append(-1)
+                    last_column = j
+                    if len(column) == 5:
+                        return board
+                else:
+                    column = []
+
+    def play(self):
+        winners = []
+        seen_winning_numbers = []
+        if not self.boards:
+            return winners
+        for bingo_number in self.input:
+            results = self.find_number_on_board(bingo_number)
+            for board_number, row_locations in results.items():
+                for location in row_locations:
+                    row_number = location["row"]
+                    column_number = location["column"]
+                    self.boards[board_number][row_number][column_number] = -1
+
+                if self.scan_column(self.boards[board_number], board_number):
+                    if bingo_number not in seen_winning_numbers:
+                        winners.append(
+                            {
+                                "winning_number": bingo_number,
+                                "board": self.boards[board_number],
+                                "board_number": board_number,
+                            }
+                        )
+                        seen_winning_numbers.append(bingo_number)
+                        del self.boards[board_number]
+                        continue
+                if (
+                    len(
+                        [
+                            tile
+                            for tile in self.boards[board_number][row_number]
+                            if tile == -1
+                        ]
+                    )
+                    == 5
+                ):
+                    if bingo_number not in seen_winning_numbers:
+                        winners.append(
+                            {
+                                "winning_number": bingo_number,
+                                "board": self.boards[board_number],
+                                "board_number": board_number,
+                            }
+                        )
+                        seen_winning_numbers.append(bingo_number)
+                        del self.boards[board_number]
+        return winners
+
+
+if __name__ == "__main__":
+    b = Bingo()
+    winning_board = b.play()
+    last_winning_board = winning_board[-1]
+    print(last_winning_board)
+    sum_of_tiles = 0
+    for row in last_winning_board["board"]:
+        for tile in row:
+            if tile != -1:
+                sum_of_tiles += int(tile)
+    print(f"Answer: {sum_of_tiles * int(last_winning_board['winning_number'])}")
